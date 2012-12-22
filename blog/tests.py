4c7091ca3fb6a77544394dc0700c40a1eb31ba43
tripples <sanketsudake@gmail.com>
2012-12-22 14:45:05 +0530

    Blog model done. Added model for blog, written test, url mapping. Basic templating done, UI should be improved. Added blog to admin interface

--- blog/tests.py
+++ blog/tests.py
@@ -6,11 +6,33 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from blog.models import Post
+from django.test.client import Client
+from django.utils import timezone
 
 
 class SimpleTest(TestCase):
+    def setUp(self):
+        self.post = Post.objects.create(post_name='ASDF',
+           post_text='Hello world',
+           pub_date=timezone.now())
+        self.client = Client()
+
     def test_basic_addition(self):
         """
         Tests that 1 + 1 always equals 2.
         """
-        self.assertEqual(1 + 1, 2)
+        self.post.save()
+        self.assertIsInstance(self.post, Post)
+
+    def test_links(self):
+        # Issue a GET request
+        response = self.client.get('/blog/')
+        # Check that the response is 200 OK
+        self.assertEqual(response.status_code, 200)
+        #print response
+        # Issue a GET request
+        response = self.client.get('/blog/1/')
+        # Check that the response is 200 OK
+        self.assertEqual(response.status_code, 200)
+        #print response

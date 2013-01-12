4abb6e4c0b1e3ce8ad625efe3a6fd2fcd5c8cc48
tripples <sanketsudake@gmail.com>
2013-01-12 17:25:33 +0530

    Shifting func. reduced views

--- codeit/views.py
+++ codeit/views.py
@@ -1,96 +1,13 @@
-from codeit.forms import UserForm, FileUploadForm
+
 from django.shortcuts import render_to_response, redirect, get_object_or_404
-from django.template import RequestContext
-from codeit.models import *
 from django.views.decorators.cache import cache_control
 from django.core.files.storage import default_storage
+from codeit.forms import UserForm, FileUploadForm
+from django.template import RequestContext
 from django.conf import settings
-from codeit.models import Post
-from django.utils import timezone
+from codeit.models import *
+from codeit.execute import *
 import os
-from codeit.models import Differ
-import commands
-
-
-def mediapath(text):
-    spath = os.path.abspath(text)
-    if 'media' in spath:
-        pass
-    else:
-        spath = spath.split("CJ")
-        spath = spath[0] + "CJ/media" + spath[1]
-    return spath
-
-
-def final_ex(sol, problem):
-    start = timezone.now()
-    code = mediapath(sol.text.name)
-    standard_input = mediapath(problem.standard_input.name)
-    standard_output = mediapath(problem.standard_output.name)
-    language = sol.language
-    print language
-    scommands = []
-
-    if language == 'c':
-        out = str(code).split(".")[0]
-        #scommand = "gcc -c " + str(code)
-        #scommands.append(scommand)
-        scommand = "gcc -o " + out + " " + str(code)
-        scommands.append(scommand)
-        scommand = "/" + out + " < " + standard_input + " > " + out + ".txt"
-        scommands.append(scommand)
-        for scommand in scommands:
-            status, output = commands.getstatusoutput(scommand)
-            if status != 0:
-                return -1
-        differ = Differ(out + ".txt", standard_output)
-        result = differ.result()
-        content = default_storage.open(out + ".txt").read()
-        total = timezone.now() - start
-        print "server time for cpp", total
-        return content
-
-    elif language == 'cpp':
-        out = str(code).split(".")[0]
-        #scommand = "g++ -c " + str(code)
-        #scommands.append(scommand)
-        scommand = "g++ -o " + out + " " + str(code)
-        scommands.append(scommand)
-        scommand = "/" + out + " < " + standard_input + " > " + out + ".txt"
-        scommands.append(scommand)
-        for scommand in scommands:
-            status, output = commands.getstatusoutput(scommand)
-            if status != 0:
-                return -1
-        differ = Differ(out + ".txt", standard_output)
-        result = differ.result()
-        content = default_storage.open(out + ".txt").read()
-        total = timezone.now() - start
-        print "server time for cpp", total
-        return content
-
-    elif language == 'java':
-        print 'Java language'
-
-    elif language == 'py':
-        out = str(code).split(".")[0]
-        scommand = "python " + str(code) + " < " + standard_input + " > " + out + ".txt"
-        scommands.append(scommand)
-        print scommand
-        for scommand in scommands:
-            status, output = commands.getstatusoutput(scommand)
-            if status != 0:
-                return -1
-        differ = Differ(out + ".txt", standard_output)
-        result = differ.result()
-        print result
-        content = default_storage.open(out + ".txt").read()
-        total = timezone.now() - start
-        print "server time constrait for python", total
-        return content
-
-    else:
-        return -1
 
 
 @login_required
@@ -112,41 +29,6 @@ def blogdetail(request, post_id):
     return render_to_response("codeit/blogdetail.html",
         {"post": p},
         context_instance=RequestContext(request))
-
-
-def getuser(receipt_no):
-    """
-    @receipt_no : Receipt no to be searched
-    Get user from receipt_no
-    Just search receipt_no as its primary key.
-    """
-    receipt_no = str(receipt_no)
-    try:
-        # Get user with exact receipt no
-        user = User.objects.get(receipt_no__exact=receipt_no)
-        if user:
-            return user
-        else:
-            print "getuser function=>User not found line no. 23"
-            return 0
-    except KeyError:
-            print "Error"
-            return 0
-
-
-def login_required(function):
-    """
-    @function : function passed for validation
-    Decorator checks before function execution
-    if user have session or not.
-    """
-    def wrapped(*args, **kw):
-        request = args[0]
-        if "receipt_no" in request.session:
-            return function(*args, **kw)
-        else:
-            return redirect("/")
-    return wrapped
 
 
 def index(request):

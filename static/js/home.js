9d2ad60ec76e4e9c78dd2ab1062df5b6a8338986
Prathamesh Sonpatki <csonpatki@gmail.com>
2013-01-12 22:48:54 +0530

    Added basic js callbacks for different types of questions

--- static/js/home.js
+++ static/js/home.js
@@ -1,13 +1,26 @@
-function changePointer(x)
-{	
-	x.style.cursor="pointer";
-	x.style.backgroundColor="#000000";
-	x.style.color="#ffffff";
+function changePointer(x) {
+    x.style.cursor="pointer";
+    x.style.backgroundColor="#000000";
+    x.style.color="#ffffff";
 }
 
 
-function changePointer2(x)
-{	
-	x.style.backgroundColor="#D3D3D3";
-	x.style.color="#000000"; 
+function changePointer2(x) {
+    x.style.backgroundColor="#D3D3D3";
+    x.style.color="#000000";
 }
+
+$(document).ready(function() {
+    // Clicked on EASY
+    $("#easy").click(function (){
+        alert("easy");
+    });
+    // Clicked on MEDIUM
+    $("#medium").click(function (){
+        alert("medium");
+    });
+    // Clicked on HARD
+    $("#hard").click(function (){
+        alert("hard");
+    });
+});

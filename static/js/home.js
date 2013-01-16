589b0c97c0cc1892a86bcbceaa28bfc7ac8a7c2c
tripples[संकेत] <sanketsudake@gmail.com>
2013-01-16 18:38:29 +0530

    Easy problems onload.

--- static/js/home.js
+++ static/js/home.js
@@ -4,7 +4,6 @@ function changePointer(x) {
     x.style.color="#ffffff";
 }
 
-
 function changePointer2(x) {
     x.style.backgroundColor="#D3D3D3";
     x.style.color="#000000";
@@ -14,6 +13,14 @@ $(document).ready(function() {
     // data is a collection of questions of a particular type
     // Iterate over it to extract fields and display them on page
     // Clicked on EASY
+     $.get('/questions/1/', function(data) {
+            $('#myproblems').empty();
+            for(var i=0; i < data.length; i++){
+                var link = (i+1) + ". " + '<a href=\"/problem/' + data[i].pk + '/\">' + data[i].fields.name;
+                link = '<br>' + link  + '</a>';
+                $('#myproblems').append(link);
+            }
+        });
     $("#easy").click(function () {
         $.get('/questions/1/', function(data) {
             $('#myproblems').empty();
@@ -47,3 +54,4 @@ $(document).ready(function() {
         });
     });
 });
+
